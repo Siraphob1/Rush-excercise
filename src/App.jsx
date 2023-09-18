caf1ea32ca770a7b93a68bb6ff782b6ebc5009a0
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-18 10:14:11 +0700

    define initial app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 
 const App = () => {
   return (
-    <div data-theme="light">
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+    <div data-theme="light" className=" min-h-screen w-full ">
+     
     </div>
   )
 }
