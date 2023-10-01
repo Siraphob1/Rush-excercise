a888f71d2a5a0217ab246f6bf9440d145886887e
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-01 17:45:21 +0700

    add useState , useRef  ,edit link to signup page

--- src/components/LoginPage/LoginInterfaceComponent.jsx
+++ src/components/LoginPage/LoginInterfaceComponent.jsx
@@ -1,22 +1,59 @@
-import React from 'react'
+
+import { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function LoginInterfaceComponent() {
+    const [userEmail , setUserEmail] = useState('');
+    const [userPassword , setUserPassword] = useState('');
+
+    const refEmail = useRef();
+    const refPassword = useRef();
+
+    
+
+    const Login = (e)=>{
+        e.preventDefault();
+        detectEmptyInput();
+    }
+
+    const detectEmptyInput = ()=>{
+        if(!userEmail) return refEmail.current.focus();
+        if(userEmail && !userPassword) return refPassword.current.focus();        
+    }
+
   return (
-        <section className=" bg-white py-[1rem] px-[2rem] w-[80%]">
-            <form className=" flex flex-col gap-y-3 ">
+        <section className=" bg-white py-[1rem] px-[2rem] w-[80%] rounded-lg">
+            <form onSubmit={(e)=> Login(e)} className=" flex flex-col gap-y-3 ">
+
+                {/* Email input */}
                 <section>
                     <label htmlFor="signup-useremail">*Email</label>
-                    <input type="text" id="signup-useremail" placeholder="example@gmail.com" className="input input-bordered block w-full " />
+                    <input  type="text" 
+                            id="signup-useremail" 
+                            ref={refEmail}
+                            placeholder="example@gmail.com" 
+                            value={userEmail} 
+                            onChange={(e)=>{setUserEmail(e.target.value)}}
+                            className="input input-bordered block w-full " />
                 </section>
+
+                {/* Password input */}
                 <section>
                     <label htmlFor="signup-userpassword">*Password</label>
-                    <input type="text" id="signup-userpassword" placeholder="password" className="input input-bordered block w-full " />
+                    <input  type="text" 
+                            id="signup-userpassword" 
+                            ref={refPassword}
+                            placeholder="password" 
+                            value={userPassword} 
+                            onChange={(e)=>{setUserPassword(e.target.value)}}
+                            className="input input-bordered block w-full " />
                 </section>
-                <section className=" flex justify-between">
-                    <button className="btn btn-accent normal-case  w-[100px] h-[30px] ">Login</button>
+
+                {/* Button */}
+                <section className=" flex justify-between py-[1rem] ">
+                    <button type='submit' className="btn  normal-case  w-[100px] h-[30px] ">Login</button>
                     <Link to={'/'} className="self-center">Forgot password?</Link>
-                    <button className="btn btn-accent normal-case  w-[100px] h-[30px] ">Sign up</button>
+                    <Link to={'/signup'} className="btn normal-case  w-[100px] h-[30px]">Sign up</Link>
                 </section>
             </form>
         </section>
