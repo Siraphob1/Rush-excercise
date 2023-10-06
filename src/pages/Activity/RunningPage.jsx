3d16b94d1b53912487c668f85565d9fa0a05bb07
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-06 12:07:43 +0700

    add image

--- src/pages/Activity/RunningPage.jsx
+++ src/pages/Activity/RunningPage.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom"
 import ActivityRemaining from "../../components/Activitylayout/ActivityRemaining"
-
+import imgRunning  from '../../assets/image/Desktop/Desktop_Running.jpeg'
 function RunningPage() {
   return (
-    <ActivityRemaining>
+    <ActivityRemaining image ={imgRunning}>
         <h1>Running</h1>
 
 
