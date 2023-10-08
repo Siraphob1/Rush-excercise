c50f0da4fe0ed83c39d0e59af82639ec88fea496
Piti-Likit <Piti.nud@gmail.com>
2023-10-08 18:46:50 +0700

    8 Oct 2023

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -5,11 +5,15 @@ const ProfilePage = () => {
 <div>
     <NavBar />
     
-    <img src ={imgProfile} className="w-full h-52" alt="Profile Page" />
-    {/* <img src="" className="rounded-full" /> */}
+    <img src ={imgProfile} className="w-full h-52 relative" alt="Profile Page" />
+     <div className="relative border border-black w-full h-52">
+    <img src="src\assets\image\Desktop\Desktop_Hiking.jpg" className="aspect-square w-[20rem] rounded-full absolute left-[50%] translate-x-[-50%] translate-y-[-50%] justify-self-center" />
+    </div>
     <div className="w-full  min-h-screen  bg-white">
-    <h1 className="m-auto" >Tang</h1>
+    <div>
+    <h1 className="m-auto" >Tang</h1> <br/>
     <h2 className="">Wannee</h2>
+    </div>
     <table>
         <thead>
             <tr>
@@ -31,3 +35,10 @@ const ProfilePage = () => {
 }
 
 export default ProfilePage;
+
+<div class="relative ...">
+  <p>Relative parent</p>
+  <div class="absolute bottom-0 left-0 ...">
+    <p>Absolute child</p>
+  </div>
+</div>
