1fe9e1447a2cdd123b1acd1b1d68a0b0950c7231
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-12 00:31:05 +0700

    create chart component

---
+++ src/components/DashboardPage.jsx/VerticalChart.jsx
@@ -0,0 +1,55 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
+
+const VerticalChart = () => {
+    const options = {
+        responsive: true,         
+        plugins: {
+          legend: {             //button toggle show graph
+            display:false,
+          },          
+        },
+        
+    };
+
+    
+    const data = {
+        labels:  ['Biking' ,'Hiking' ,'Running' ,'Swimming' ,'Walking'],
+        datasets: [
+          {
+            label: 'count',
+            data: [10,1,2,3,5],
+            backgroundColor: ['rgba(0, 0, 0)','rgba(189, 255, 0)' ,'rgba(204, 204, 204)' , 'rgba(189, 252, 165)' , 'rgba(255, 235, 165)']
+          },        
+        ],
+      };
+      
+
+  return (
+    <section className='w-full border'>
+        <h1 className='text-black font-semibold mb-[1rem]'>Number of Activities</h1>
+        <Bar options={options} data={data}/>
+    </section>
+  )
+}
+
+export default VerticalChart
