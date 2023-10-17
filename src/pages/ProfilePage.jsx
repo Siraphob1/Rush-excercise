1fc1c13574f41e991603105c74e0a702c97779e5
Piti-Likit <Piti.nud@gmail.com>
2023-10-17 15:34:27 +0700

    17-10-23

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -6,30 +6,30 @@ const ProfilePage = () => {
     <NavBar />
     
     <img src ={imgProfile} className="w-full h-52 relative" alt="Profile Page" />
-     <div className="relative border border-black w-full h-52">
+    <div className="relative border border-black w-full h-52">
     <img src="src\assets\image\Desktop\Desktop_Hiking.jpg" className="aspect-square w-[20rem] rounded-full absolute left-[50%] translate-x-[-50%] translate-y-[-50%] justify-self-center" />
     </div>
     <div className="w-full  min-h-screen  bg-white">
-    <div>
-    <h1 className="m-auto" >Tang</h1> <br/>
-    <h2 className="">Wannee</h2>
+        <div className="flex justify-center">
+             <h1 className="m-auto text-black text-4xl font-bold" >Tang</h1>
+        </div>
+        <div className="text-black text-4xl font-bold flex justify-center">
+             <h2 className="">Wannee Mairuukinarai</h2>
+        </div>
+        <table className="table-fixed flex justify-center">
+            <thead>
+                <tr>
+                    <td> age</td>
+                    <td>height</td>
+                    <td>weight</td>
+                </tr> 
+            </thead>
+            <tbody>            
+            </tbody>
+        </table>
+        <br />
+        <h2 className="text-black text-4xl font-bold flex justify-start">About</h2>
     </div>
-    <table>
-        <thead>
-            <tr>
-                <td> age</td>
-                <td>height</td>
-                <td>weight</td>
-            </tr> 
-        </thead>
-        <tbody>
-            
-        </tbody>
-    </table>
-    <br />
-    <h2>About</h2>
-    </div>
-
 </div>
     )
 }
