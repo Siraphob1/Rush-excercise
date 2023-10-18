4c788567aa2269c314ec25e6d1a2e353219ce516
Piti-Likit <Piti.nud@gmail.com>
2023-10-18 10:17:04 +0700

    18/10 UI profile page 2nd update

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,12 +3,11 @@ import imgProfile from '../assets/image/Desktop/Desktop_BgProfile.jpg'
 const ProfilePage = () => {
     return (
 <div>
-    <NavBar />
-    
+    <NavBar />    
     <img src ={imgProfile} className="w-full h-52 relative" alt="Profile Page" />
-    <div className="relative border border-black w-full h-52">
-    <img src="src\assets\image\Desktop\Desktop_Hiking.jpg" className="aspect-square w-[20rem] rounded-full absolute left-[50%] translate-x-[-50%] translate-y-[-50%] justify-self-center" />
-    </div>
+      <div className="relative border border-black w-full h-52">
+        <img src="src\assets\image\Desktop\Desktop_Hiking.jpg" className="aspect-square w-[20rem] rounded-full absolute left-[50%] translate-x-[-50%] translate-y-[-50%] justify-self-center" />
+      </div>
     <div className="w-full  min-h-screen  bg-white">
         <div className="flex justify-center">
              <h1 className="m-auto text-black text-4xl font-bold" >Tang</h1>
@@ -16,29 +15,35 @@ const ProfilePage = () => {
         <div className="text-black text-4xl font-bold flex justify-center">
              <h2 className="">Wannee Mairuukinarai</h2>
         </div>
-        <div className="justify-center stats stats-vertical lg:stats-horizontal shadow">
-  
-  <div className="stat">
-    <div className="stat-title">22</div>
-    <div className="stat-value">Age</div>
-    {/* <div className="stat-desc">Jan 1st - Feb 1st</div> */}
-  </div>
-  
-  <div className="stat">
-    <div className="stat-title">175</div>
-    <div className="stat-value">Height</div>
-    {/* <div className="stat-desc">↗︎ 400 (22%)</div> */}
-  </div>
-  
-  <div className="stat">
-    <div className="stat-title">67</div>
-    <div className="stat-value">Weight</div>
-    {/* <div className="stat-desc">↘︎ 90 (14%)</div> */}
-  </div>
-  
-</div>
+
+        <div className="ml-80 flex justify-center stats stats-vertical lg:stats-horizontal shadow">
+            <div className="stat">
+              <div className="stat-title">22</div>
+              <div className="stat-value">Age</div>
+                {/* <div className="stat-desc">Jan 1st - Feb 1st</div> */}
+              </div>
+          
+          <div className="stat">
+              <div className="stat-title">175</div>
+              <div className="stat-value">Height</div>
+                {/* <div className="stat-desc">↗︎ 400 (22%)</div> */}
+              </div>
+          
+          <div className="stat">
+            <div className="stat-title">67</div>
+            <div className="stat-value">Weight</div>
+            {/* <div className="stat-desc">↘︎ 90 (14%)</div> */}
+          </div>  
+        </div>
         <br />
-        <h2 className="text-black text-4xl font-bold flex justify-start">About</h2>
+      <h2 className="text-black text-4xl font-bold flex justify-start">About</h2>
+    <div className="container w-full bg-white border border-black w-full h-52">
+      <div className="p-10 text-3xl font-bold font-black">
+        <p>สวัสดีชาวโลก</p>
+        <br></br>
+        <p>ชอบวิ่ง  ชอบปีนเขา</p>
+      </div>
+    </div>
     </div>
 </div>
     )
@@ -47,7 +52,7 @@ const ProfilePage = () => {
 export default ProfilePage;
 
 <div class="relative ...">
-  <p>Relative parent</p>
+    <p>Relative parent</p>
   <div class="absolute bottom-0 left-0 ...">
     <p>Absolute child</p>
   </div>
