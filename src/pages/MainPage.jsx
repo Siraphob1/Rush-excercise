2a81479603092320fc513478cf6396002f112ef7
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-25 03:37:24 +0700

    add API

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,7 +5,10 @@ import { Nav } from "../components/MainPage/Nav";
 // import { From } from "../components/MainPage/From";
 import { Card } from "../components/MainPage/Card";
 import { From } from "../components/MainPage/From";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import  { axiosPrivate } from "../api/axios";
 
 export const MainPage = () => {
   const initdata = [
@@ -23,12 +26,50 @@ export const MainPage = () => {
     // },
   ];
 
+  const {auth , activity , setActivity} = useAuth();
+  const ACTIVITY_URL = `/api/activity/${auth?.userID}`
+  const navigate = useNavigate();
+  const location = useLocation();
   const [items, setItems] = useState(initdata);
+  const rawactivityList  = activity?.activityList || [];
+  const [activitiesList , setActivityList] = useState([]);
+  const [updateFinished , setUpdateFinished] = useState(false);
   const onAddNewCard = (newItem) => {
     setItems((prevItems) => {
       return [newItem, ...prevItems];
     });
   };
+
+  useEffect(()=>{
+      
+      const getActivities = async () => {
+        try {
+            
+            const response = await axiosPrivate.get(ACTIVITY_URL, {
+                headers: {"Authorization" : `Bearer ${auth?.accessToken}`}
+            });
+            console.log(response.data);
+            setActivity({...activity , activityList:response.data.activitiesList})
+            // isFecthing && setUsers(response.data);
+            setUpdateFinished(true)
+            
+        } catch (err) {
+            console.error(err.response);
+            navigate('/login' , {state: {from:location} , replace:true})
+        }
+      }
+
+      if(updateFinished) return
+      getActivities(); 
+      
+      
+      
+  },[activity])
+
+  useEffect(()=>{
+    const filterActivity = rawactivityList.filter((e)=> e.status === "inprogress");
+    setActivityList(filterActivity);
+  },[])
 
   return (
     <main className="w-full  main-screen" data-theme="light">
@@ -52,11 +93,11 @@ export const MainPage = () => {
             {/* กรอกข้อมูลและแสดงผล */}
             <div>
               <div className="flex justify-center my-5">
-                <From onAddItem={onAddNewCard} />
+                <From onAddItem={onAddNewCard} API_URL ={ACTIVITY_URL} location={location} />
               </div>
               <div className=" grid grid-cols-4 gap-4 mx-10">
-                {items.map((element) => {
-                  return <Card {...element} key={element.id} />;
+                {activitiesList.map((element) => {
+                  return <Card {...element} key={element.activityID} />;
                 })}
               </div>
             </div>
