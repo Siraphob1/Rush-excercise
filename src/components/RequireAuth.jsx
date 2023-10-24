894032e1e629718c2db61f0365c0f8e11c87830c
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-24 13:13:30 +0700

    catch when token expire

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -1,15 +1,39 @@
 /* eslint-disable react/prop-types */
-import { Navigate, useLocation } from "react-router";
+import { Navigate, useLocation , useNavigate } from "react-router";
 import useAuth from "../hooks/useAuth"
+import { useEffect, useState } from "react";
+import useRefreshToken from "../hooks/useRefreshToken";
+
 
 const RequireAuth = ({children}) => {
     const {auth} = useAuth();
+    const refresh = useRefreshToken();
     const location = useLocation();
+    const navigate = useNavigate();
+    const [refreshedFinished , setRefreshedFinished] = useState(false);
+    useEffect(()=>{
+      
+      const printnewToken = async () =>{
+        try {
+
+          //get newaccessToken sucess
+          const newtoken = await refresh();              
+          console.log(`newtoken: ${newtoken}`);
+          console.log(location)
+          setRefreshedFinished(true)
+        } catch (error) {
+          //token expired redirect to login page
+          navigate('/login')
+        }
+      }
+
+      printnewToken();
+    },[])
   return (
     <>
-        {
-            auth?.accessToken   ? children
-                                : <Navigate to="/login" state={{ from: location }} replace />
+        {!refreshedFinished  ? children
+                            : auth?.accessToken   ? children
+                                                  : <Navigate to="/login" state={{ from: location }} replace />
         }
     </>
   )
