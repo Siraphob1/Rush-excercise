2518be253a455acecfe11a67ab3c94151fe4bc93
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-24 18:32:08 +0700

    remove  process request for get newaccessToken

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -1,39 +1,15 @@
 /* eslint-disable react/prop-types */
-import { Navigate, useLocation , useNavigate } from "react-router";
+import { Navigate, useLocation } from "react-router";
 import useAuth from "../hooks/useAuth"
-import { useEffect, useState } from "react";
-import useRefreshToken from "../hooks/useRefreshToken";
-
 
 const RequireAuth = ({children}) => {
     const {auth} = useAuth();
-    const refresh = useRefreshToken();
     const location = useLocation();
-    const navigate = useNavigate();
-    const [refreshedFinished , setRefreshedFinished] = useState(false);
-    useEffect(()=>{
-      
-      const printnewToken = async () =>{
-        try {
-
-          //get newaccessToken sucess
-          const newtoken = await refresh();              
-          console.log(`newtoken: ${newtoken}`);
-          console.log(location)
-          setRefreshedFinished(true)
-        } catch (error) {
-          //token expired redirect to login page
-          navigate('/login')
-        }
-      }
-
-      printnewToken();
-    },[])
+   
   return (
     <>
-        {!refreshedFinished  ? children
-                            : auth?.accessToken   ? children
-                                                  : <Navigate to="/login" state={{ from: location }} replace />
+        {auth?.accessToken  ? children
+                            : <Navigate to="/login" state={{ from: location }} replace />
         }
     </>
   )
