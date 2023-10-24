d0ddbed7f143545d87ff06874b63125f9dad1027
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-24 17:18:36 +0700

    update nama of variable to equal backend require

--- src/components/MainPage/Card.jsx
+++ src/components/MainPage/Card.jsx
@@ -6,7 +6,7 @@ import imgSwimming from '../../assets/image/Desktop/Desktop_Swimming.jpg'
 import imgRunning from '../../assets/image/Desktop/Desktop_Running.jpeg'
 import { useEffect, useState } from 'react'
 export const Card = (props) => {
-  const {about,activity,} = props
+  const {id , name , description,type, startDate , endDate , createDate} = props
 
   const [imgBG , setImgBG] = useState('');
   const [pos, setPos] = useState('');
@@ -14,7 +14,7 @@ export const Card = (props) => {
   const [classtopic , setClassTopic] = useState('');
 
   useEffect(()=>{
-    const typeActivty = activity;
+    const typeActivty = type;
 
     switch (typeActivty) {
       case "Biking" :   setImgBG(imgBiking);  
@@ -50,6 +50,10 @@ export const Card = (props) => {
     console.log(imgBG)
   },[])
 
+  const showData = () =>{
+    console.log(`${id} / ${name} / ${description} / ${type} / ${startDate} / ${endDate} / ${createDate}`)
+  }
+
   return (
     <div>
       <div className="card w-[20rem] h-[10rem] bg-base-100 shadow-xl rounded-3xl"
@@ -61,9 +65,11 @@ export const Card = (props) => {
           }}
       >
         
-        <div className={`card-body  flex flex-col items-end ${classtopic}  rounded-3xl hover:bg-black hover:bg-opacity-40 hover:cursor-pointer`}>
-          <h1 className="card-title uppercase font-bold pt-[0.5rem] "> {activity} </h1>
-          <p className=""> {about}</p>
+        <div className={`card-body  flex flex-col items-end ${classtopic}  rounded-3xl hover:bg-black hover:bg-opacity-40 hover:cursor-pointer`}
+        onClick={showData}
+        >
+          <h1 className="card-title uppercase font-bold pt-[0.5rem] "> {type} </h1>
+          <p className=""> {description}</p>
 
         </div>
       </div>
