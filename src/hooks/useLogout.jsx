a4a9e9e5d39193555607331c7597e667f92bab2d
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-25 10:03:52 +0700

    clear data when logout

--- src/hooks/useLogout.jsx
+++ src/hooks/useLogout.jsx
@@ -2,12 +2,15 @@ import axiosPublic from "../api/axios";
 import useAuth from "./useAuth";
 
 const useLogout = () => {
-    const {setAuth} = useAuth();
+    const {setAuth , setActivity} = useAuth();
 
     const logout = async () =>{
             setAuth({});
             try {
                 const response = await axiosPublic.get('/logout');
+                //clear
+                setAuth({});
+                setActivity({});
             } catch (error) {
                 console.error(error);
             }
